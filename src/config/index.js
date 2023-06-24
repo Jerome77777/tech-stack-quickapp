4a6a1e7d9a3d2e34b988aeb92e120ee288ed81cb
jerome777 <jerome_77777@163.com>
2023-06-24 19:21:16 +0800

    删除node_modules

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,5 @@
 const files = require.context('.', true, /\.js$/)
 const configs = {}
 
-files.keys().forEach(key => {
-  if (key === './index.js') return
-  configs[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
-})
 
 export default configs
